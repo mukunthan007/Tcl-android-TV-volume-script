1e0fe9caa0145e90eac9aa0078d8c23908ac5b4b
mukunthank <mukunthan2001@gmail.com>
2024-11-20 23:29:17 +0530

    minor changes

--- volume-bash.sh
+++ volume-bash.sh
@@ -8,7 +8,7 @@ FIRST_COMMAND=("$ADB_PATH" "shell" "settings" "put" "secure" "enabled_accessibil
 SECOND_COMMAND=("$ADB_PATH" "shell" "settings" "put" "secure" "enabled_accessibility_services" "dev.vodik7.tvquickactions/dev.vodik7.tvquickactions.KeyAccessibilityService")
 
 # Define regex pattern for volume up/down events
-VOLUME_PATTERN="KEYCODE_VOLUME_(UP|DOWN)"
+VOLUME_PATTERN="KEYCODE_VOLUME_"
 
 # Function to get current time for filtering logs
 get_current_time() {
@@ -24,15 +24,15 @@ while true; do
     CURRENT_TIME=$(get_current_time)
 
     # Start logcat and process each line
-    "$ADB_PATH" logcat -T "$CURRENT_TIME" | while read -r line; do
-        if [[ "$line" =~ $VOLUME_PATTERN ]]; then
-            echo "Volume button pressed. Executing first command..."
+    stdbuf -oL "$ADB_PATH" logcat -T "$CURRENT_TIME" | while read -r line; do
+        if [[ "$line" == *$VOLUME_PATTERN* ]]; then
+            #echo "Volume button pressed. Executing first command..."
             "${FIRST_COMMAND[@]}"
 
-            echo "Waiting 10 seconds..."
+            #echo "Waiting 10 seconds..."
             sleep 10
 
-            echo "Executing second command..."
+            #echo "Executing second command..."
             "${SECOND_COMMAND[@]}"
 
             # Clear logcat and restart monitoring
