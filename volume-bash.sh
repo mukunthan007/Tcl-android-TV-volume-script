50f66312236166e9c98aa0299e71876d6561b075
Mukunthan <50894477+mukunthan007@users.noreply.github.com>
2025-01-04 03:06:34 +0530

    Update volume-bash.sh

--- volume-bash.sh
+++ volume-bash.sh
@@ -5,8 +5,9 @@ ADB_PATH="/data/data/com.termux/files/usr/bin/adb"  # Replace with the correct p
 
 # Define commands
 FIRST_COMMAND=("$ADB_PATH" "shell" "settings" "put" "secure" "enabled_accessibility_services" "None")
-SECOND_COMMAND=("$ADB_PATH" "shell" "settings" "put" "secure" "enabled_accessibility_services" "dev.vodik7.tvquickactions/dev.vodik7.tvquickactions.KeyAccessibilityService")
-
+TVQUICKPRO_COMMAND="dev.vodik7.tvquickactions/dev.vodik7.tvquickactions.KeyAccessibilityService"
+DISPATCH_COMMAND="com.spauldhaliwal.dispatch/com.spauldhaliwal.dispatch.services.DispatchHomeService"
+COMBINED_SERVICES="$TVQUICKPRO_COMMAND:$DISPATCH_COMMAND"
 # Define regex pattern for volume up/down events
 VOLUME_PATTERN="KEYCODE_VOLUME_"
 
@@ -24,16 +25,16 @@ while true; do
     CURRENT_TIME=$(get_current_time)
 
     # Start logcat and process each line
-    stdbuf -oL "$ADB_PATH" logcat -T "$CURRENT_TIME" | while read -r line; do
-        if [[ "$line" == *$VOLUME_PATTERN* ]]; then
+    "$ADB_PATH" logcat -T "$CURRENT_TIME" | while read -r line; do
+        if [[ "$line" =~ $VOLUME_PATTERN ]]; then
             #echo "Volume button pressed. Executing first command..."
             "${FIRST_COMMAND[@]}"
 
-            #echo "Waiting 10 seconds..."
+            echo "Waiting 10 seconds..."
             sleep 10
 
-            #echo "Executing second command..."
-            "${SECOND_COMMAND[@]}"
+            echo "Executing second command..."
+            "$ADB_PATH" shell settings put secure enabled_accessibility_services "$COMBINED_SERVICES"
 
             # Clear logcat and restart monitoring
             "$ADB_PATH" logcat -c
